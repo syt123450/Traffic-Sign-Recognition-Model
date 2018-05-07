93ad13ad3c606f67705346749e54c0750ba7a052
Chenhua Zhu <zchholmes@gmail.com>
2018-05-06 17:46:57 -0700

    add plot

--- predict43/model/f_analysis.py
+++ predict43/model/f_analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def read_in_file(file_name):
     with open(file_name) as fh:
@@ -35,3 +36,10 @@ if __name__ == "__main__":
     print(avg_pre_list)
     print(avg_rec_list)
     print(avg_f_list)
+
+    x_marks = ["Origin", "Impv1", "Impv2", "Impv3", "Impv4", "Impv5"]
+    # plt.plot(x_marks, avg_acc_list, 'r-', label="Avg. Accuracy")
+    plt.plot(x_marks, avg_pre_list, 'b-', label="Avg. Precision")
+    plt.plot(x_marks, avg_rec_list, 'g-', label="Avg. Recall")
+    plt.plot(x_marks, avg_f_list, 'r-', label="Avg. F Score")
+    plt.show()
